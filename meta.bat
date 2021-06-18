c4c7fb669bc5b83d8142b7163cb507ce7402d814
AntonC9018 <anton.curm@gmail.com>
2021-06-18 16:55:48 +0300

    fix batch script

--- meta.bat
+++ meta.bat
@@ -1,16 +1,34 @@
 @echo off
 
+if "%~1" == ""      goto view
+if "%~1" == "all"   goto all
+if "%~1" == "fresh" goto fresh
+
+:fresh
+echo Restoring afresh
+cd Hopper
+call meta all
+cd ..
 cd View
-call dotnet restore
+call dotnet build
 cd ..
+goto view
 
+:view
+echo Restoring just the view
 cd Hopper/Meta
-
-if %1%==all (
-    dotnet run Hopper.Core;../../Hopper/Core/Hopper.Core.csproj Hopper.TestContent;../../Hopper/TestContent/Hopper.TestContent.csproj Hopper.View;../../View/Hopper.View.csproj
-) else (
-    dotnet run Hopper.Core Hopper.TestContent Hopper.View;../../View/Hopper.View.csproj
-)
-
+dotnet run Hopper.Core Hopper.TestContent Hopper.View;../../View/Hopper.View.csproj
 cd ..
 cd ..
+goto the_end
+
+:all
+echo Restoring both the model and the view
+cd Hopper/Meta
+dotnet run Hopper.Core;../../Hopper/Core/Hopper.Core.csproj Hopper.TestContent;../../Hopper/TestContent/Hopper.TestContent.csproj Hopper.View;../../View/Hopper.View.csproj
+cd ..
+cd ..
+
+goto the_end
+
+:the_end
